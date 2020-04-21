693ac8f694b65b16cb37657d13cf1ba1affc0fab
Daniel Lwetabe <acehanks@gmail.com>
2020-04-21 16:26:37 +0300

    confirmed is working

--- disbursments/main.php
+++ disbursments/main.php
@@ -16,21 +16,7 @@ $client = new Client([
     'timeout'  => 20.0,
 ]);
 $uuid = Uuid::uuid4();
-$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImYzMDE0OGQ2LTk5NmQtNGUyNy04MTI1LTNmN2JlYjBkYWViZSIsImV4cGlyZXMiOiIyMDIwLTA0LTE5VDE1OjAyOjI3LjE3MyIsInNlc3Npb25JZCI6IjAyNDEwYzUxLWUxNGItNGQzZi05ZjMyLTY3MTIwYzgyOWVjYyJ9.TpqRKtfc1LZuRpJTwCPhfUfACxtAJH2Dumbx5BJnK9EazzEsunbRVHJ9fQwSniw4FM7aZuILdFXgZRpKmz3zs9tjh64gtltVxCcLYUoFiQi69gWrTx5R-1OYPsm6pGd9I-Ry9nVeFOFQbPw5_tEmwIlUxMCZLn0fc9nnJKA5g9JoNPqdrOura0EBYKjmBqGE5KoqUbWgbVYIUchoSR_AF4Z0SzcDQSHbbypf0Ybr8QawqOljasIr8cuF8rpzYis8GQzGI6LTNivtPmWANjCcQe0nce9ILML3SQUsABlfRgmM4ppZujkO8ChlMwLig5YJTdhz-gVqMsRduvEzz-7Wyg";
-$options = [
-    'json' => [
-        "amount" => "5000.0",
-        "currency" => "EUR",
-        "externalId" => "1241235246039",
-        "payee" => array(
-            "partyIdType" => "MSISDN",
-            "partyId" => "2379524"
-        ),
-        "payerMessage" => "Make bulk payment",
-        "payeeNote" => "yeah",
-    ]
-]; 
-echo "------------------------------\n";
+$token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImYzMDE0OGQ2LTk5NmQtNGUyNy04MTI1LTNmN2JlYjBkYWViZSIsImV4cGlyZXMiOiIyMDIwLTA0LTIxVDE0OjA1OjIwLjE1NyIsInNlc3Npb25JZCI6ImFlOWRlZDBkLWU2MDQtNGM1Mi1hNmQ2LWM3MTIyNDk1NWZkOSJ9.QBAtbkEg3XTTdS3Ut8m7_CnP-6xpkojLBP8-p6206QBvwJjOCkuD-Bq3tH-_Pj_dRb25isw6Q9VYYcvrecIULraoiH1BW61MGN0mcvSnayFpElTOluZImpvGqtP-OC1R7-DRIUkeAlFFuNAdUAh_vowiSnaO8Hzpx6h8JXThhIxI90Jw0o2QHeXXFjb2hIvNE28cBt9vklAX2TZzNTPANsTO-LcWYI6OiNJLyHDuZP5qYOqBR0x0fxL_u0jsMfQN906czffxFdQZZevaz3qWEZL_uwccE5ao04g97puhThOdynqkLGVn42I-1hXR60PjQLj2uR_M9DFtBD9m0pCNtg";
 
 $response = $client->request("POST","/disbursement/v1_0/transfer", [
     RequestOptions::HEADERS => [
@@ -53,8 +39,7 @@ $response = $client->request("POST","/disbursement/v1_0/transfer", [
     ]
 ]);
 
-echo "------------------------------";
+echo "------------------------------\n";
 echo $response->getStatusCode();
 echo $response->getBody();
-echo $response->getHeader('application/json');
 echo "------------------------------";
