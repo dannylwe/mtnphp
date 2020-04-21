a9471492c3f0ef4de41d41c9821ec2a2022ac83e
Daniel Lwetabe <acehanks@gmail.com>
2020-04-21 19:41:02 +0300

    add try catch block

--- disbursments/Key/apikey.php
+++ disbursments/Key/apikey.php
@@ -8,29 +8,31 @@ header('Content-Type: application/json');
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
 use GuzzleHttp\Exception\ClientException;
-use Ramsey\Uuid\Uuid;
 
 function getApiKey() {
     $refId = "f30148d6-996d-4e27-8125-3f7beb0daebe";
     $subscriptionKey = "819f567702324c2d9b2f2a143a1fd591";
-    $client = new Client([
+    $clientKey = new Client([
         // Base URI is used with relative requests
         'base_uri' => 'https://sandbox.momodeveloper.mtn.com/',
         // You can set any number of default request options.
         'timeout'  => 20.0,
     ]);
-
-    $response = $client->request("POST","v1_0/apiuser/$refId/apikey", [
-        RequestOptions::HEADERS => [
-            'Accept' => 'application/json',
-            "Ocp-Apim-Subscription-Key" => $subscriptionKey,
-            ],
-    ]);
-    $apiBody = json_decode($response->getBody());
-    $key = $apiBody->apiKey;
-    if($response->getStatusCode() == 201) {
-        return $key;
-    }
+    try {
+        $response = $clientKey->request("POST","v1_0/apiuser/$refId/apikey", [
+            RequestOptions::HEADERS => [
+                'Accept' => 'application/json',
+                "Ocp-Apim-Subscription-Key" => $subscriptionKey,
+                ],
+        ]);
+        $apiBody = json_decode($response->getBody());
+        $key = $apiBody->apiKey;
+        if($response->getStatusCode() == 201) {
+            return $key;
+        }
+    } catch (ClientException $e) {
+        echo json_encode(array("statusError" => "Connection Error to MTN"));
+    }   
 }
 
 // echo getApiKey();
--- disbursments/Request/main.php
+++ disbursments/Request/main.php
@@ -3,8 +3,14 @@
 namespace App\Request;
 
 require '../../vendor/autoload.php';
+require_once '../key/apikey.php';
 
 header('Content-Type: application/json');
+
+use App\Key;
+
+$apiKey = Key\getApiKey();
+// echo $apiKey;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
@@ -20,28 +26,33 @@ $client = new Client([
 $uuid = Uuid::uuid4();
 $token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSMjU2In0.eyJjbGllbnRJZCI6ImYzMDE0OGQ2LTk5NmQtNGUyNy04MTI1LTNmN2JlYjBkYWViZSIsImV4cGlyZXMiOiIyMDIwLTA0LTIxVDE1OjA3OjMxLjQwNCIsInNlc3Npb25JZCI6Ijc3ZGM4ZDZjLWFiZmYtNDI3MC1iNDU5LTQ0NzllMmExMTk5OCJ9.jzQPf2kYloAsNDLsxEikq5aTiEb1rgQ3QOx3V5Ztb17YHLUJr6ULhF863Weuz0Ab3A7_fmw6-HbOXO9Be2Z7PJQEcvZ1QZS48-398lPM6CeZVFhuS_KPEoCNoIT2O5X8b6KfTlINeQ1EHXiX7JcVlGjMja7Rd15MAi_dvDtiCxPAOzN4pTDALyw08WN45gF_ry_p-h3kI764bI42qKLfmVtDujOjTeItlmrEJ_AuyFQYBoE2-RDGNCon8ZBhKs_I0SbYFIHmYW6FCiD7hskc8Z0HqyspKbILoFiHXmGsHxOiEWB5x8KWumKa4ICEQK7hlHIzin_1QVvpnV4WZgcJVQ";
 
-$response = $client->request("POST","/disbursement/v1_0/transfer", [
-    RequestOptions::HEADERS => [
-        'Accept' => 'application/json',
-        "Ocp-Apim-Subscription-Key" => "819f567702324c2d9b2f2a143a1fd591",
-        'X-Reference-Id' => $uuid->toString(),
-        'X-Target-Environment' => 'sandbox',
-        'Authorization' => 'Bearer ' . $token
-        ],
-    RequestOptions::JSON => [
-        "amount" => "5000.0",
-        "currency" => "EUR",
-        "externalId" => "1241235246039",
-        "payee" => array(
-            "partyIdType" => "MSISDN",
-            "partyId" => "2379524"
-        ),
-        "payerMessage" => "Make bulk payment",
-        "payeeNote" => "yeah"
-    ]
-]);
+try {
+    $response = $client->request("POST","/disbursement/v1_0/transfer", [
+        RequestOptions::HEADERS => [
+            'Accept' => 'application/json',
+            "Ocp-Apim-Subscription-Key" => "819f567702324c2d9b2f2a143a1fd591",
+            'X-Reference-Id' => $uuid->toString(),
+            'X-Target-Environment' => 'sandbox',
+            'Authorization' => 'Bearer ' . $token
+            ],
+        RequestOptions::JSON => [
+            "amount" => "5000.0",
+            "currency" => "EUR",
+            "externalId" => "1241235246039",
+            "payee" => array(
+                "partyIdType" => "MSISDN",
+                "partyId" => "2379524"
+            ),
+            "payerMessage" => "Make bulk payment",
+            "payeeNote" => "yeah"
+        ]
+    ]);
+    echo $response->getStatusCode();
+    echo $response->getBody();
+    
+} catch (ClientException $e) {
+    echo json_encode(array("statusError" => "token has expired"));
+}
 
-echo "------------------------------\n";
-echo $response->getStatusCode();
-echo $response->getBody();
-echo "------------------------------";
+
+
